4deb24a575a96412aab82622234a01d8aca65eb8
mcorbin <corbin.math@gmail.com>
2017-05-25 17:32:05 +0200

    Add i3

--- link.sh
+++ link.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-ln -s .dotfiles/.bashrc .bashrc
-ln -s .dotfiles/.Xdefaults .Xdefaults
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.Xdefaults ~/.Xdefaults
+ln -s ~/.dotfiles/i3_config ~/.config/i3/config
